002ffac39f55fa07d891cdb745243c307063e8c8
Nasr.Maswood <nasrmaswood@gmail.com>
2020-08-09 12:49:22 -0400

    maybe ask nari for vpc help

--- aws_learn/sql/__init__.py
+++ aws_learn/sql/__init__.py
@@ -7,15 +7,10 @@ from aws_learn.sql.Constants import RDS_URL, RDS_PORT, RDS_USERNAME, RDS_PASSWOR
 def connect(database: str):
     print(RDS_URL, RDS_PORT, database, RDS_USERNAME, RDS_PASSWORD)
 
-    x = f"postgresql://{RDS_URL}/TEST?user={RDS_USERNAME}&password={RDS_PASSWORD}"
-
-    return psycopg2.connect(x)
+    return psycopg2.connect(
+        f"postgresql://{RDS_URL}/TEST?user={RDS_USERNAME}&password={RDS_PASSWORD}"
+    )
 
 
 if __name__ == "__main__":
-    "postgresql:///mydb?user=other&password=secret"
-
-    print("hi")
     connection = connect("TEST")
-    breakpoint()
-    pass
