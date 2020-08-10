8db9451e2315f2196b40b138d5fe6758d27cba7f
Nasr.Maswood <nasrmaswood@gmail.com>
2020-08-10 11:22:53 -0400

    start working on chalice

--- aws_learn/aws_learn/chalice/app.py
+++
@@ -1,8 +0,0 @@
-from chalice import Chalice
-
-app = Chalice(app_name="aws_learn/chalice")
-
-
-@app.route("/")
-def index():
-    return {"hello": "world"}
--- chalice/app.py
+++ chalice/app.py
@@ -1,10 +1,21 @@
+import os
+
 from chalice import Chalice
 import aws_learn as A
 
-app = Chalice(app_name="chalice")
+app = Chalice(app_name="39 Queue")
 
 
-@app.route("/")
+@app.route("/ping")
 def index():
-    A.kafka.produce()
-    return {"something": "is on the queue"}
+    return "pong"
+
+
+QUEUE_NAME = os.environ["SQS_QUEUE_NAME"]
+
+
+@app.on_sqs_message(queue=QUEUE_NAME)
+def handler(event):
+    for record in event:
+        print(record
+
